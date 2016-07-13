528bbb97eb10308482f9d06a02f651dbc175589a
bakfall <251738041@qq.com>
2016-07-13 18:02:20 +0800

    713pm

---
+++ js/data.js
@@ -0,0 +1,73 @@
+var users = [
+	{
+		nickname : "玩家一",
+		id : null,
+		win : 0, 
+		total : 0,
+		status : 1
+	},
+	{
+		nickname : "玩家一",
+		id : null,
+		win : 0, 
+		total : 0,
+		status : 1
+	},
+	{
+		nickname : "玩家一",
+		id : null,
+		win : 0, 
+		total : 0,
+		status : 1
+	},
+	{
+		nickname : "玩家一",
+		id : null,
+		win : 0, 
+		total : 0,
+		status : 1
+	},
+	{
+		nickname : "玩家一",
+		id : null,
+		win : 0, 
+		total : 0,
+		status : 1
+	},
+	{
+		nickname : "玩家一",
+		id : null,
+		win : 0, 
+		total : 0,
+		status : 1
+	},
+	{
+		nickname : "玩家一",
+		id : null,
+		win : 0, 
+		total : 0,
+		status : 1
+	},	
+]
+var rooms = [
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+	{roomname : "0"},
+]
+
